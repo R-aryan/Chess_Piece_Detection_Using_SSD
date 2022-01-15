79d2b9760dada487379399c9ca63f432942ce59f
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2022-01-15 16:17:40 +0530

    minor code refactoring in inference pipeline and html templetes

--- services/chess_piece_detector/application/ai/inference/prediction.py
+++ services/chess_piece_detector/application/ai/inference/prediction.py
@@ -1,28 +1,23 @@
 import numpy as np
-import argparse
-import os
 import tensorflow as tf
 from PIL import Image
 from io import BytesIO
 from datetime import datetime
-import pathlib
-import glob
-import matplotlib.pyplot as plt
 import cv2
 
 from object_detection.utils import label_map_util
 from object_detection.utils import ops as utils_ops
 from object_detection.utils import visualization_utils as vis_util
 
-# patch tf1 into `utils.ops`
-from utils.utils import encodeImageIntoBase64
 
+from utils.utils import encodeImageIntoBase64
+from services.chess_piece_detector.settings import Settings
+
+# patch tf1 into `utils.ops`
 utils_ops.tf = tf.compat.v1
 
 # Patch the location of gfile
 tf.gfile = tf.io.gfile
-
-from services.chess_piece_detector.settings import Settings
 
 
 class ChessDetector:
